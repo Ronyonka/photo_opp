e039db4ee42fde3ba95019c77b9a9f3df3ad2e33
Ronyonka <rononyonka@gmail.com>
2019-02-28 12:12:54 +0300

    added admin page

--- photos/admin.py
+++ photos/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Location,Image,category
 
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal = ('category')
+
 admin.site.register(Location)
-admin.site.register(Image)
+admin.site.register(Image,ImageAdmin)
 admin.site.register(category)
