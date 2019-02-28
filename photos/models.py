0d0288fa50b01c215de61d29a8ee460b5489932f
Ronyonka <rononyonka@gmail.com>
2019-02-28 11:26:15 +0300

    added an image model

--- photos/models.py
+++ photos/models.py
@@ -11,3 +11,10 @@ class category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Image(models.Model):
+    image_name = models.CharField(max_length=30)
+    description = models.CharField(max_length=30)
+    # photo = models.ImageField(upoad_to='images/')
+    location = models.ForeignKey(Location)
+    category = models.ManyToManyField(category)
