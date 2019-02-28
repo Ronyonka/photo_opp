13a1c493ab464c8df4cef3108c0ef5d198a7674c
Ronyonka <rononyonka@gmail.com>
2019-02-28 11:19:51 +0300

    created category

--- photos/models.py
+++ photos/models.py
@@ -2,4 +2,12 @@ from django.db import models
 
 class Location(models.Model):
     location_name = models.CharField(max_length=30)
-    
+
+    def __str__(self):
+        return self.location_name
+
+class category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
