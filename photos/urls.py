698671f173e10d35adc1d5dd52e6addf06c1abcb
Ronyonka <rononyonka@gmail.com>
2019-02-28 12:54:56 +0300

    added search form in the category

--- photos/urls.py
+++ photos/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.all_photos,name='all_photos')
+    url('^$',views.all_photos,name='all_photos'),
+    url(r'^search/', views.search_results, name='search_results'),
 ]
