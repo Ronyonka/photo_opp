a5894a11f1e4dd6f7e0bf9eb182e077c9d3d144f
Ronyonka <rononyonka@gmail.com>
2019-03-03 15:01:14 +0300

    added class methods to image class

--- photos/models.py
+++ photos/models.py
@@ -39,13 +39,44 @@ class Image(models.Model):
     category = models.ManyToManyField(Category)
     image_url = models.ImageField(upload_to = 'articles/')
 
-    @classmethod
-    def get_photos(cls):
-        return cls.objects.all()
+  def save_image(self):
+       self.save()
 
-    @classmethod
-    def search_by_category(cls,category):
-        images = Category.objects.filter(name__icontains=category)
+   def delete_image(self):
+       Image.objects.get(id = self.id).delete()
+
+   def update_image(self,val):
+       Image.objects.filter(id = self.id).update(name = val)
+
+   @classmethod
+   def get_image_by_id(cls,image_id):
+       return cls.objects.get(id = image_id)
+    
+   @classmethod
+   def copy_image(image_url):
+        find_image = Image.get_image_by_id(image_id)
+        return pyperclip.copy(find_image.image_url)
+
+   @classmethod
+   def show_image(cls,category):
+        images = cls.objects.filter(category__name=category)
         return images
 
+        
+   @classmethod
+   def get_photos(cls):
+       return cls.objects.all()
 
+
+   @classmethod
+   def search_by_category(cls,category):
+        photo = Category.objects.filter(name__icontains = category)[0]
+        return  cls.objects.filter(category_id = photo.id)
+
+   @classmethod
+   def filter_by_location(cls,location):
+       the_location = Location.objects.get(name = location)
+       return cls.objects.filter(location_id = the_location.id)
+
+   def __str__(self):
+       return self.name
